534101d03ebab1b17ef82381b57b43081bba05b9
morteza <mgahangir2006@gmail.com>
2023-02-20 23:42:22 +0330

    Navbar_Css--config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
- import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
- import { CTA, Brand, Navbar } from './components';
-
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="gradiant__bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
