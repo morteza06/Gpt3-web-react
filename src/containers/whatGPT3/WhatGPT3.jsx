a9326e3769256c5617b72ea373930ac515e1bbc7
morteza <mgahangir2006@gmail.com>
2023-02-21 14:04:40 +0330

    Whatgpt3-Component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div>
-        WhatGPT3
+    <div className="gpt3__whatgpt3 section_margin" id='whatgpt3'>
+      <div className="gpt3__whatgp3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
