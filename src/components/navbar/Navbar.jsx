da02fee2ee7c4c9aef07b2c7ac6a497d770f3eed
morteza <mgahangir2006@gmail.com>
2023-02-21 23:41:57 +0330

    Features-Component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt">What is GPT?</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
@@ -34,7 +34,7 @@ const Navbar = () => {
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <p><a href="#home">Home</a></p>
-              <p><a href="#wgpt">What is GPT?</a></p>
+              <p><a href="#wgpt3">What is GPT?</a></p>
               <p><a href="#possibility">Open AI</a></p>
               <p><a href="#features">Case Studies</a></p>
               <p><a href="#blog">Library</a></p>
